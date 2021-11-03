7c665cd253ddb49d61c35e4a66270565b31f0ecb
unknown <kimitawanjohi7923@gmail.com>
2021-11-03 18:13:16 +0300

    nav down

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
+
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Layout>
     </Router>
   );
 }
---
+++ src/components/Layout/Navbar.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import {
+    makeStyles,
+    AppBar,
+    Toolbar,
+    InputBase,
+    Typography,
+    Avatar,
+    Badge,
+    IconButton
+} from '@material-ui/core';
+import {
+     Search,
+     AddShoppingCart,
+     Cancel
+} from '@material-ui/icons';
+import { grey } from '@material-ui/core/colors';
+
+const useStyles = makeStyles( theme => ({
+    logo: {
+        marginRight: theme.spacing(1)
+    },
+    toolbar: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    badges: {
+        display: 'flex',
+        alignItems: 'center',
+        [theme.breakpoints.down("sm")]: {
+            display: (show) => {
+                if(!show){
+                    return "flex"
+                }else{
+                    return "none"
+                }
+            }
+        }  
+    },
+    badge: {
+        marginLeft: theme.spacing(2)
+    },
+    search: {
+        display: "flex",
+        alignItems: "center",
+        background: grey[100],
+        width: theme.spacing(50),
+        borderRadius: theme.shape.borderRadius,
+        [theme.breakpoints.down("md")]: {
+            display: (show) => {
+                if(show){
+                    return "flex"
+                }else{
+                    return "none"
+                }
+            }
+        }
+    },
+    searchInput: {
+        marginLeft: theme.spacing(1),
+        flexGrow: "1"
+    }, 
+    cancel: {
+        [theme.breakpoints.up("md")]: {
+            display: "none"
+        }
+    }
+}));
+
+export default function Navbar () {
+    const [show, setShow] = useState(false)
+    const classes = useStyles(show);
+
+    return (
+        <>
+            <AppBar color="inherit">
+                <Toolbar className={classes.toolbar}>
+                    <Typography variant="h5" component="p" className={classes.logo}>
+                        logo
+                    </Typography>
+                    <div className={classes.search}>
+                        <Search />
+                        <InputBase placeholder="Search..."  className={classes.searchInput} />
+                        <Cancel className={classes.cancel} onClick={() => {setShow(false)}} />
+                    </div>
+                    <div className={classes.badges}>
+                        <IconButton onClick={() => setShow(true)} >
+                            <Search className={classes.cancel} />
+                        </IconButton>
+                        <Badge badgeContent={2} color='secondary' className={classes.badge}>
+                            <AddShoppingCart />
+                        </Badge> 
+                        <Avatar className={classes.badge}> A</Avatar>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
