097851f28bf23f019d3875b968f9d85e401c1a3c
Kimita Kibana Wanjohi <kimitawanjohi7923@gmail.com>
2021-11-11 21:13:18 +0300

    Revert "productdetails component almost"

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -70,11 +70,10 @@ function Sidebar() {
                     button
                     className={classes.menuItems}
                      >
-                  <img className={classes.categoryImage} src={item.image} alt="" />
+                  <img className={classes.categoryImage} src={item.image} />
                         <ListItemText style={{marginLeft:'3px'}}>{item.text}</ListItemText>
                         <ListItemIcon> <ChevronRight color="secondary" /> </ListItemIcon>
                     </ListItem>
-
                 ))}
             </List>
         </div>
