92f7a329d6dbe760f4867d1be7f89b723d0bd189
unknown <kimitawanjohi7923@gmail.com>
2021-11-17 09:44:04 +0300

    cart still

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,6 +1,6 @@
-import React, {useContext} from 'react';
-import {makeStyles, TableCell, TableRow, IconButton} from "@material-ui/core";
-import { DeleteForeverOutlined } from '@material-ui/icons';
+import React, {useContext, useState} from 'react';
+import {makeStyles, TableCell, TableRow, IconButton, ButtonGroup, Typography} from "@material-ui/core";
+import { DeleteForeverOutlined, AddOutlined,  Remove} from '@material-ui/icons';
 import {useMutation} from '@apollo/client';
 import {CartContext} from '../../Context/CartContext';
 import {REMOVE_ITEM} from '../../graphql/mutation';
@@ -9,11 +9,20 @@ const useStyles = makeStyles(theme => ({
     cartImage: {
         maxWidth: theme.spacing(8),
         height: 'auto'
+    },
+    quantity:{
+        display: 'flex',
+        alignItems: 'center'
+    }, 
+    text: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1)
     }
 }));
 
 export default function CartItem({item}) {
     const {setCart} = useContext(CartContext);
+    const [quantity, setQuantity] = useState()
     const [removeItem] = useMutation(REMOVE_ITEM);
     const classes = useStyles();
     return (
@@ -23,7 +32,17 @@ export default function CartItem({item}) {
             </TableCell>
             <TableCell align="left">{item.product.name}</TableCell>
             <TableCell align="left">{item.unitPrice}</TableCell>
-            <TableCell align="left">{item.quantity}</TableCell>
+            <TableCell align="left">
+                <div className={classes.quantity}>
+                        <IconButton size="small" color="secondary" edge="end" >
+                            <Remove/>
+                        </IconButton>
+                        <Typography className={classes.text} > {item.quantity} </Typography>
+                        <IconButton size="small" color="secondary" edge="start" >
+                            <AddOutlined />
+                        </IconButton>
+                </div>
+            </TableCell>
             <TableCell align="left">{item.total}</TableCell>
             <TableCell align="left">
                 <IconButton onClick={async ()=>{
